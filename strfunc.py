46ea85e53b9b25bd2a31f2ae205b73ef8b438990
Barnav Pran Kalita <barnavpran12345@gmail.com>
2024-03-13 23:54:39 +0530

    Add files via upload

---
+++ strfunc.py
@@ -0,0 +1,17 @@
+#str.capitalize()
+# capitalises the first index value
+
+#str.replace(old,new)
+#replaces all the substrings or letters from a string
+
+#str.count()
+#counts the number of occurances of a sub str in a str
+
+str="this is a simple string"
+
+str=str.capitalize()
+print(str)
+str.replace("t","o")
+print(str)
+a=str.count("i")
+print(a)
